aea76f7d62944935d9d2a627611f14b66b18d46b
Austin Tanner <austin.tanner04@gmail.com>
2017-09-14 03:04:35 -0500

    redesign of ui

--- function.js
+++ function.js
@@ -30,50 +30,87 @@ weatherApp.controller('weatherCtrl', function($scope, $http){
                 vm. humidity = data.main.humidity;
                 vm. description = data.weather[0].description;
                 vm. summary = data.weather[0].main;
-                vm. icon = "http://openweathermap.org/img/w/"+ data.weather[0].icon + ".png";
+                vm. model = data.weather[0].icon;
                 vm. cTemp = convertToCelcius(vm.fTemp);
                 vm. cTempHigh = convertToCelcius(vm.fTempHigh);
                 vm. cTempLow = convertToCelcius(vm.fTempLow);
                 vm. city = data.name;
 
-                console.log(data.weather[0].main);
+                console.log(vm.model);
 
-                switch(vm.description){
-          case "clear day":
+                switch(vm.model){
+          case "01d":
             vm.image = 'https://i.pinimg.com/originals/fe/ee/42/feee42fdf12266d5a2e6e9aabcacfb28.jpg';
-            vm.color = 'black';
+            vm.icon = '<i class="wi-day-sunny"></i>';
             break;
-          case "scattered clouds":
+          case "01n":
+            vm.image = 'http://i.imgur.com/nwqHT3H.jpg';
+            vm.icon = '<i class="wi-night-clear"></i>';
+            vm.b = 'well'
+            break;
+          case "03d":
             vm.image = 'http://2.bp.blogspot.com/-xVDyriNbwGM/Tl9Au5dDjpI/AAAAAAAAKJM/bQayN2Yd2Yc/s1600/sunset%2BIMG_9750.jpg';
-            vm.color = 'white';
+            vm.icon = '<i class="wi-day-cloudy"></i>';
             break;
-          case "rain":
+          case "03n":
+            vm.image = 'https://www.walldevil.com/wallpapers/a82/moon-cloud-night.jpg';
+            vm.icon = '<i class="wi-night-alt-cloudy"></i>';
+            break;
+          case "10d":
             vm.image = 'http://52.24.98.51/wp-content/uploads/2017/03/rain.jpg';
-            vm.color = 'black';
+            vm.icon = '<i class="wi-day-sprinkle"></i>';
             break;
-          case "snow":
+          case "10n":
+            vm.image = 'https://www.walldevil.com/wallpapers/a82/moon-cloud-night.jpg';
+            vm.icon = '<i class="wi-night-alt-showers"></i>';
+            break;
+          case "13d":
             vm.image = 'https://previews.123rf.com/images/coleong/coleong0801/coleong080100533/2434677-Stock-image-of-a-snowing-winter-at-Boston-Massachusetts-USA-Stock-Photo.jpg';
-            vm.color = 'black';
+            vm.icon = '<i class="wi-day-snow"></i>';
             break;
-          case "shower rain":
+          case "13n":
+            vm.image = 'http://allswalls.com/images/snow-winter-night-sky-wallpaper-1.jpg';
+            vm.icon = '<i class="wi-night-alt-snow"></i>';
+            break;
+          case "09d":
             vm.image = 'http://www.bttoronto.ca/wp-content/uploads/sites/2/2016/08/heavyrain-1024x576.jpg';
-            vm.color = 'black';
+            vm.icon = '<i class="wi-day-rain"></i>';
             break;
-          case "mist":
+          case "09n":
+            vm.image = 'https://i.ytimg.com/vi/q76bMs-NwRk/maxresdefault.jpg';
+            vm.icon = '<i class="wi-night-alt-rain"></i>';
+            break;
+          case "50d":
             vm.image = 'http://www.metoffice.gov.uk/binaries/content/gallery/mohippo/images/migrated-image/9/walkers-486583_1920_1.jpg)';
-            vm.color = 'black';
+            vm.icon = '<i class="wi-day-fog"></i>';
             break;
-          case "few clouds":
+          case "50n":
+            vm.image = 'http://2.bp.blogspot.com/-UHSbJrVpwHg/UMmbntniPtI/AAAAAAAAAaA/Sf2vdyq2r2w/s1600/DSC_4290+copy.jpg';
+            vm.icon = '<i class="wi-night-fog"></i>';
+            break;
+          case "02d":
             vm.image = "https://i.ytimg.com/vi/8NTVWJRNvv0/maxresdefault.jpg";
-            vm.color = 'black';
+            vm.icon = '<i class="wi-day-sunny-overcast"></i>';
             break;
-          case "broken clouds":
+          case "02n":
+            vm.image = 'https://i.ytimg.com/vi/cHcQ6iEIQLI/maxresdefault.jpg';
+            vm.icon = '<i class="wi-night-alt-partly-cloudy"></i>';
+            break;
+          case "04d":
             vm.image = 'https://img13.deviantart.net/fa39/i/2015/052/6/1/broken_clouds_by_leo_6tos-d8ixdlv.jpg';
-            vm.color = 'black';
+            vm.icon = '<i class="wi-cloudy"></i>';
             break;
-          case "thunderstorm":
+          case "04n":
+            vm.image = 'http://gatherfraservalley.ca/wp-content/uploads/2017/05/file8081258144701.jpg';
+            vm.icon = '<i class="wi-cloudy"></i>';
+            break;
+          case "11d":
             vm.image = 'https://23711-presscdn-pagely.netdna-ssl.com/wp-content/uploads/2015/06/Thunderstorm-5best.jpg';
-            vm.color = 'white';
+            vm.icon = '<i class="wi-day-storm-showers"></i>';
+            break;
+          case "11n":
+            vm.image = 'http://www.wallpaperup.com/632942/nature_night_sky_lightning_sea_ocean_storm_rain.html';
+            vm.icon = '<i class="wi-night-alt-thunderstorm"></i>';
             break;
                       }
 
